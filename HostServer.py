9e702901080ebe1fbf110a552103569df532b98a
mkarier <mkarier@eagles.ewu.edu>
2019-01-21 19:23:36 -0800

    It was running normally on Linux but was throwing an error on windows. I just had to encode the strings before sending it through the ports. now it works on both Windows and Linux.

--- HostServer.py
+++ HostServer.py
@@ -4,8 +4,8 @@ import os
 import time
 import vlc
 
-host ='192.168.50.43'
-port = '9998'
+host ="192.168.50.43"
+port = "9998"
 
 def PlayVideo(filepath,raspberry, clientSocket):
 	try:
@@ -15,10 +15,10 @@ def PlayVideo(filepath,raspberry, clientSocket):
 		vlcMedia = vlcInstance.media_new(filepath, option)
 		vlcPlayer.set_media(vlcMedia)
 		vlcPlayer.play()
-		print("Is about to enter the while loop")
+		print("Now starting reading from socket")
 		clientSocket.setblocking(1)
-		command = clientSocket.recv(1024)
-		while (command not in "quit") and (vlcPlayer.get_position() < 1.0):
+		command = str(clientSocket.recv(1024))
+		while ('quit' not in command) and (vlcPlayer.get_position() < 1.0):
 			if 'pause' in command:
 				vlcPlayer.pause()
 			elif 'play' in command:
@@ -26,7 +26,7 @@ def PlayVideo(filepath,raspberry, clientSocket):
 			elif 'stop' in command:
 				vlcPlayer.stop()
 			try:
-				command = clientSocket.recv(1024)
+				command = str(clientSocket.recv(1024))
 			except:
 				command = 'continue'
 		print("About to return to main thread")
@@ -42,12 +42,12 @@ def main():
 	listenPort = 9000
 	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serverSocket.bind((host, listenPort))
-	print "host ip: " + host
+	print("host ip: " + host)
 	serverSocket.listen(1)
 	clientSocket, raspberryTuple = serverSocket.accept()
 	raspberry = raspberryTuple[0]
-	clientSocket.send(port)
-	clientSocket.send(str(numberOfVideos -1))
+	clientSocket.send(port.encode())
+	clientSocket.send(str(numberOfVideos -1).encode())
 	try:
 		for videoIndex in range(1, numberOfVideos):
 			PlayVideo(sys.argv[videoIndex], raspberry,clientSocket)
